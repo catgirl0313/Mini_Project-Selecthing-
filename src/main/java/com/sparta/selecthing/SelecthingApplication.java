493da239846ea78ee630d4ebf6e096896db14f1d
Jiyoung Baek <vvvv4449@gmail.com>
2022-07-23 14:52:11 +0900

    Initial commit Mini Project Selecthing

---
+++ src/main/java/com/sparta/selecthing/SelecthingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.selecthing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecthingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelecthingApplication.class, args);
+    }
+
+}
